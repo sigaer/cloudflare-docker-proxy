20a59947f3928feac019d10f1abb239a73e13615
Ichika <73508033+sigaer@users.noreply.github.com>
2024-07-11 16:37:02 +0800

    Update index.js
    change routes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.mikuware.top": dockerHub,
+  "quay.mikuware.top": "https://quay.io",
+  "gcr.mikuware.top": "https://gcr.io",
+  "k8s-gcr.mikuware.top": "https://k8s.gcr.io",
+  "k8s.mikuware.top": "https://registry.k8s.io",
+  "ghcr.mikuware.top": "https://ghcr.io",
+  "cloudsmith.mikuware.top": "https://docker.cloudsmith.io",
+  "ecr.mikuware.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.mikuware.top": dockerHub,
 };
 
 function routeByHosts(host) {
